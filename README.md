ee6b5c290406e2b522783d0cf16e136ab71f19ac
Chris DuBois <chris@graphlab.com>
2014-08-01 11:43:59 -0700

    Add Yelp loading example.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ how-to
 ======
 
 [Convert a column of date strings into UNIX timestamps](convert_column_to_timestamp.py)
+[Load Yelp academic dataset](load_yelp_dataset.py)
---
+++ load_yelp_dataset.py
@@ -0,0 +1,17 @@
+import graphlab
+
+business = graphlab.SFrame.read_csv('yelp_academic_dataset_business.json', header=False, delimiter='\n', column_type_hints=dict)
+checkin = graphlab.SFrame.read_csv('yelp_academic_dataset_checkin.json', header=False, delimiter='\n', column_type_hints=dict)
+review = graphlab.SFrame.read_csv('yelp_academic_dataset_review.json', header=False, delimiter='\n', column_type_hints=dict)
+user = graphlab.SFrame.read_csv('yelp_academic_dataset_user.json', header=False, delimiter='\n', column_type_hints=dict)
+tip = graphlab.SFrame.read_csv('yelp_academic_dataset_tip.json', header=False, delimiter='\n', column_type_hints=dict)
+
+# Changing JSON into tables, i.e. SFrames
+reviews = review.unpack('X1', new_column_name_prefix='')
+businesses = business.unpack('X1', new_column_name_prefix='', limit=['business_id', 'name', 'latitude', 'longitude', 'stars'])
+
+# Build a recommender system
+m = graphlab.recommender.create(reviews, 'user_id', 'business_id')
+
+# Find businesses that are similar based on users in common
+m.get_similar_items(['BVxlrYWgmi-8TPGMe6CTpg']).join(businesses, on={'similar_item':'business_id'})
