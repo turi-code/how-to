4af6730873c1335599811acded191db98faa8599
Ping Wang <ping@graphlab.com>
2014-08-13 15:50:36 -0700

    SFrame.unpack() interface change

--- load_yelp_dataset.py
+++ load_yelp_dataset.py
@@ -7,8 +7,8 @@ user = graphlab.SFrame.read_csv('yelp_academic_dataset_user.json', header=False,
 tip = graphlab.SFrame.read_csv('yelp_academic_dataset_tip.json', header=False, delimiter='\n', column_type_hints=dict)
 
 # Changing JSON into tables, i.e. SFrames
-reviews = review.unpack('X1', new_column_name_prefix='')
-businesses = business.unpack('X1', new_column_name_prefix='', limit=['business_id', 'name', 'latitude', 'longitude', 'stars'])
+reviews = review.unpack('X1', column_name_prefix='')
+businesses = business.unpack('X1', column_name_prefix='', limit=['business_id', 'name', 'latitude', 'longitude', 'stars'])
 
 # Build a recommender system
 m = graphlab.recommender.create(reviews, 'user_id', 'business_id')
