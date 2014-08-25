611573eee6988e1d4c7a6bf0e41eb6b5f4e334d3
srikris <srikris@graphlab.com>
2014-08-25 10:50:51 -0700

    Made some minor spacing changes.

--- select_subset_rows.py
+++ select_subset_rows.py
@@ -10,20 +10,17 @@ sf = gl.SFrame({
 
 # Select only rows with positive flight_time.
 # ----------------------------------------------------------------------------
+# Note: Method 1 will be faster than Method 2, but Method 2 is more flexible.
+
 # Method 1: SFrame Logical Filter
 sf_filter = sf[sf['flight_time'] > 0] 
-
 # Method 2: SFrame apply() function
 sf_filter = sf[sf['flight_time'] > 0] 
 
-# Note: Method 1 will be faster than Method 2, but Method 2 is more flexible.
-
 # Select flights from either carriers 'US' or 'LY' or plane type 727
 # ----------------------------------------------------------------------------
-
 # Using Method 1
 sf_filter = sf[(sf['carrier'] == 'US') | (sf['carrier'] == 'LY') | (sf['plane_type'] == 727)]
-
 # Using Method 2
 sf_filter = sf[sf.apply(lambda x: True if (x['carrier'] == 'US' or x['carrier'] == 'LY' or x['plane_type'] == 727) else False)]
 
