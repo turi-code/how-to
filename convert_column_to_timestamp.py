eb8de1696a6f626a402aaea6c80fc8515a04641e
srikris <srikris@graphlab.com>
2014-08-26 12:13:47 -0700

    Made everything 80 chars and consistent with format.

--- convert_column_to_timestamp.py
+++ convert_column_to_timestamp.py
@@ -1,7 +1,6 @@
-# Title: Convert a date string column to a UNIX timestamp
+# Requires you to 'pip install python-dateutil==1.5'
 import graphlab as gl
 from datetime import datetime
-# Requires you to 'pip install python-dateutil==1.5'
 from dateutil import parser
 
 def str_to_timestamp(the_str):
@@ -9,10 +8,11 @@ def str_to_timestamp(the_str):
         dt = parser.parse(the_str)
     except:
         return None
-
     # UNIX epoch is January 1, 1970
     return (dt - datetime(1970,1,1)).total_seconds()
 
 # 02/29/2001 is invalid, so should be 'None' in output
-sf = gl.SFrame({'date':['2000-08-21','2013-06-08 17:25:00.12753','02/29/2001'],'id':[1,2,3]})
+sf = gl.SFrame({
+        'date':['2000-08-21','2013-06-08 17:25:00.12753','02/29/2001'],
+        'id':[1,2,3]})
 sf['date'] = sf['date'].apply(str_to_timestamp)
--- load_yelp_dataset.py
+++ load_yelp_dataset.py
@@ -1,17 +1,26 @@
-import graphlab
+import graphlab as gl
 
-business = graphlab.SFrame.read_csv('yelp_academic_dataset_business.json', header=False, delimiter='\n', column_type_hints=dict)
-checkin = graphlab.SFrame.read_csv('yelp_academic_dataset_checkin.json', header=False, delimiter='\n', column_type_hints=dict)
-review = graphlab.SFrame.read_csv('yelp_academic_dataset_review.json', header=False, delimiter='\n', column_type_hints=dict)
-user = graphlab.SFrame.read_csv('yelp_academic_dataset_user.json', header=False, delimiter='\n', column_type_hints=dict)
-tip = graphlab.SFrame.read_csv('yelp_academic_dataset_tip.json', header=False, delimiter='\n', column_type_hints=dict)
+# Data available to download from 
+# https://www.yelp.com/academic_dataset
+business = gl.SFrame.read_csv('yelp_academic_dataset_business.json', 
+                header=False, delimiter='\n', column_type_hints=dict)
+checkin = gl.SFrame.read_csv('yelp_academic_dataset_checkin.json', 
+                header=False, delimiter='\n', column_type_hints=dict)
+review = gl.SFrame.read_csv('yelp_academic_dataset_review.json', 
+                header=False, delimiter='\n', column_type_hints=dict)
+user = gl.SFrame.read_csv('yelp_academic_dataset_user.json', 
+                header=False, delimiter='\n', column_type_hints=dict)
+tip = gl.SFrame.read_csv('yelp_academic_dataset_tip.json', 
+                header=False, delimiter='\n', column_type_hints=dict)
 
 # Changing JSON into tables, i.e. SFrames
 reviews = review.unpack('X1', column_name_prefix='')
-businesses = business.unpack('X1', column_name_prefix='', limit=['business_id', 'name', 'latitude', 'longitude', 'stars'])
+businesses = business.unpack('X1', column_name_prefix='', 
+              limit=['business_id', 'name', 'latitude', 'longitude', 'stars'])
 
 # Build a recommender system
-m = graphlab.recommender.create(reviews, 'user_id', 'business_id')
+m = gl.recommender.create(reviews, 'user_id', 'business_id')
 
 # Find businesses that are similar based on users in common
-m.get_similar_items(['BVxlrYWgmi-8TPGMe6CTpg']).join(businesses, on={'similar_item':'business_id'})
+similar_items = m.get_similar_items(['BVxlrYWgmi-8TPGMe6CTpg'])
+print similar_items.join(businesses, on={'similar_item':'business_id'})
