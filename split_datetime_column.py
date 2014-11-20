e07e58d3d0419584963359b05912f64ec0fd0c34
Srikrishna Sridhar <srikris@graphlab.com>
2014-11-20 15:35:53 -0800

    Update split_datetime_column.py

--- split_datetime_column.py
+++ split_datetime_column.py
@@ -1,16 +1,17 @@
-# Title: Split a datetime column into year, month, and timezone
-
+# Title: Parse a datetime column into its components (year, month, etc.)
 import graphlab as gl
 from graphlab_util.timezone import GMT
 from datetime import datetime
 
+# Define the inputs
 d1 = datetime(2011, 1, 21, 7, 7, 21, tzinfo=GMT(0))
 d2 = datetime(2010, 2, 5, 7, 8, 21, tzinfo=GMT(4.5))
 
+# The SArray can store native datetime objects.
 sa = gl.SArray([d1,d2])
-res = sa.split_datetime(column_name_prefix="",limit=['year','month'],tzone=True)
-print res
 
+result = sa.split_datetime(column_name_prefix="",limit=['year','month'],tzone=True)
+print result
 #+------+-------+-------+
 #| year | month | tzone |
 #+------+-------+-------+
@@ -19,9 +20,8 @@ print res
 #+------+-------+-------+
 #[2 rows x 3 columns]
 
-res = sa.split_datetime(column_name_prefix="res",limit=['day','hour','minute','second'])
-print res
-
+result = sa.split_datetime(column_name_prefix="res",limit=['day','hour','minute','second'])
+print result
 #+---------+----------+------------+------------+
 #| res.day | res.hour | res.minute | res.second |
 #+---------+----------+------------+------------+
