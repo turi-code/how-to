ef7712cb10023284b37447d0436dd200dcedc6cc
Robert Voyer <robert@graphlab.com>
2015-03-03 10:42:09 -0800

    Added some more documentation and updated README

--- extract_article_content_from_HTML.py
+++ extract_article_content_from_HTML.py
@@ -1,16 +1,62 @@
-# Extract article content from HTML with Boilerpipe
 # Be sure to install boilerpipe (pip install boilerpipe)
+"""
+Extract main article content from HTML with Boilerpipe
+
+In this script, we define a function that takes a single HTML news article as
+input and returns the primary textual content of the article. We create an
+SFrame from a sample of HTML articles, and then we pass the function defined
+above to the apply method on the column in our SFrame containing the HTML
+source. The clean content is included in our SFrame as a new column. We use the
+Python boilerpipe module to extract the clean text.
+"""
 import graphlab as gl
-import codecs
 import os
 import requests
-import subprocess
 from StringIO import StringIO
 from urllib import urlopen
 from zipfile import ZipFile
 
-# Define function to return clean content given HTML source as string
 def extract_article_content(src_content):
+    """
+    Extract the primary textual content from an HTML news article.
+
+    In many cases, the HTML source of news articles is littered with boilerplate
+    text that you would not want to include when doing text analysis on the
+    content the page. Even if you could write some rules to extract the content
+    from one page, it's unlikely that those rules would apply to an article from
+    another site. The boilerpipe module allows us to solve this problem more
+    generally.
+
+    Parameters
+    ----------
+    src_conent : str
+        The source HTML from which to extract the content.
+
+    Returns
+    -------
+    out : str
+        The primary content of the page with all HTML and boilerplate text
+        removed.
+
+    Examples
+    --------
+    >>> extract_article_content(
+            "<html><body><p>Dato is in the business of building the best " \
+            "machine learning platform on the planet. Our goal is to make it " \
+            "easy for data scientists to build intelligent, predictive " \
+            "applications quickly and at scale. Given the perplexing array " \
+            "of tools in this space, we often get asked "Why Dato? What " \
+            "differentiates it from tools X, Y, and Z?" This blog post aims " \
+            "to provide some answers. I’ll go into some technical details " \
+            "about the challenges of building a predictive application, and " \
+            "how Dato’s ML platform can help.</p></body></html>")
+
+
+    See Also
+    --------
+    - `Boilerpipe project <https://code.google.com/p/boilerpipe/>`_
+    - `Boilerpipe Python module <https://pypi.python.org/pypi/boilerpipe>`_
+    """
     import logging
     from boilerpipe.extract import Extractor
 
